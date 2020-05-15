9aaf89758d8e14753cf23c49ae2546ad9a229f3f
zengwenhao <zengwenhao@deepexi.com>
2020-05-15 09:41:21 +0800

    feat: 新增角色权限码

--- deepexi-spring-cloud-provider/src/main/java/com/deepexi/enums/PermissionCode.java
+++ deepexi-spring-cloud-provider/src/main/java/com/deepexi/enums/PermissionCode.java
@@ -4,6 +4,10 @@ public enum PermissionCode {
 
 
     ASSIGN("role:assign", "分配角色"),
+    CREATE("rode:create","创建角色"),
+    UPDATE("role:update","更新角色"),
+    VIEW("role:view","查看角色"),
+    DELETE("rode:delete","删除角色"),
     API_VIEW("api:view","接口信息");
 
 
